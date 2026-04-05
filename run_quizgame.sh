426e337e92412abeef79dce5f42f35677f948e31
Ubuntu <ubuntu@ip-172-31-25-70.eu-north-1.compute.internal>
2026-04-05 23:50:32 +0000

    edit code

--- run_quizgame.sh
+++ run_quizgame.sh
@@ -5,4 +5,4 @@ apt install python3-pip python3-venv -y
 python3 -m venv venv2
 source venv2/bin/activate
 pip install streamlit
-streamlit run quizgame/testquiz_app.py
+streamlit run quizgame/quiz_app.py
